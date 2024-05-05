ca543b4fc01c6f54deec24453dd874db12ed65b5
Fer Acevedo <nandoad86@hotmail.com>
2024-05-05 13:30:19 -0600

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -15,11 +15,12 @@ internal class Program
             opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
         });
 
+        builder.Services.AddCors();
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
 
-
+        app.UseCors(build => build.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
         app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
